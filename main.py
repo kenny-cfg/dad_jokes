6d16734a00479f86428c4577c77ff47f4619c733
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-21 20:29:12 +0100

    Extract text from JSON response

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import requests
 
 def get_joke():
     response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
-    print(response.text)
-    # json_response = response.json()
-    # print(json_response)
+    json_response = response.json()
+    joke_text = json_response['joke']
+    print(joke_text)
 
 
 if __name__ == '__main__':
