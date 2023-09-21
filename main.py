d4b1f0c0d3ef090c0eaac480c375f0382a4ea336
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-21 20:27:30 +0100

    Get the response in JSON

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 
 
 def get_joke():
-    response = requests.get('https://icanhazdadjoke.com/')
-    print(response)
+    response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+    print(response.text)
+    # json_response = response.json()
+    # print(json_response)
 
 
 if __name__ == '__main__':
