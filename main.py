be503a394860d56f5728100f26669bf4d1eab6cb
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-21 20:31:38 +0100

    Write joke text to file

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ def get_joke():
     response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
     json_response = response.json()
     joke_text = json_response['joke']
-    print(joke_text)
+    return joke_text
+
+
+def write_to_file(text):
+    with open('joke.txt', 'a') as joke_file:
+        joke_file.write(text)
+        joke_file.write('\n')
 
 
 if __name__ == '__main__':
-    get_joke()
+    joke = get_joke()
+    write_to_file(joke)
